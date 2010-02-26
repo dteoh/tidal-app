d9b91c70d311e52e8979b0892275d230803b726e
dteoh <dteoh@tidal-app.org>
2010-02-27 09:14:37 +1000

    BugzID:15 - Added GPLv3 License.

--- src/main/java/org/tidal_app/tidal/impl/sources/gmail/GmailDroplet.java
+++ src/main/java/org/tidal_app/tidal/impl/sources/gmail/GmailDroplet.java
@@ -1,3 +1,19 @@
+/*
+ * Tidal, a communications aggregation and notification tool. 
+ * Copyright (C) 2010 Douglas Teoh 
+ * 
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details. You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.tidal_app.tidal.impl.sources.gmail;
 
 import java.io.IOException;
@@ -15,7 +31,6 @@ import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.mail.Store;
 import javax.mail.search.FlagTerm;
-
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/main/java/org/tidal_app/tidal/views/RippleView.java
+++ src/main/java/org/tidal_app/tidal/views/RippleView.java
@@ -1,3 +1,19 @@
+/*
+ * Tidal, a communications aggregation and notification tool. 
+ * Copyright (C) 2010 Douglas Teoh 
+ * 
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details. You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.tidal_app.tidal.views;
 
 import java.awt.Color;
@@ -13,9 +29,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.text.html.HTMLDocument;
 
-import org.tidal_app.tidal.views.models.DropletContentModel;
-
 import net.miginfocom.swing.MigLayout;
+
+import org.tidal_app.tidal.views.models.DropletContentModel;
 
 public class RippleView extends JPanel implements Comparable<RippleView> {
 
