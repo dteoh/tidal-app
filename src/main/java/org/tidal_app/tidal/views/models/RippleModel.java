99c20184ef25b94a90a2fce24c6848ce82682ab9
dteoh <dteoh@tidal-app.org>
2010-09-12 16:46:25 +1000

    Javadoc.

--- src/main/java/org/tidal_app/tidal/views/models/RippleModel.java
+++ src/main/java/org/tidal_app/tidal/views/models/RippleModel.java
@@ -16,8 +16,15 @@
 
 package org.tidal_app.tidal.views.models;
 
+/**
+ * View model for representing a single data feed item.
+ * 
+ * @author Douglas Teoh
+ * 
+ */
 public final class RippleModel implements Comparable<RippleModel> {
 
+    /** Utility class for creating the model using builder pattern. */
     public static class Builder {
         private final Object id;
         private String origin;
@@ -25,30 +32,64 @@ public final class RippleModel implements Comparable<RippleModel> {
         private String content;
         private long received;
 
+        /**
+         * Creates a new builder.
+         * 
+         * @param id
+         *            identifier to assign to the built model.
+         */
         public Builder(final Object id) {
             this.id = id;
         }
 
+        /**
+         * Origin of the data feed item.
+         * 
+         * @param origin
+         * @return this
+         */
         public Builder origin(final String origin) {
             this.origin = origin;
             return this;
         }
 
+        /**
+         * Subject of the data feed item.
+         * 
+         * @param subject
+         * @return this
+         */
         public Builder subject(final String subject) {
             this.subject = subject;
             return this;
         }
 
+        /**
+         * Contents of the data feed item.
+         * 
+         * @param content
+         * @return this
+         */
         public Builder content(final String content) {
             this.content = content;
             return this;
         }
 
+        /**
+         * Time since the epoch of when the data feed item was received.
+         * 
+         * @param received
+         * @return this
+         */
         public Builder received(final long received) {
             this.received = received;
             return this;
         }
 
+        /**
+         * Creates the ripple model using the information passed to this
+         * builder.
+         */
         public RippleModel build() {
             return new RippleModel(id, origin, subject, content, received);
         }
@@ -81,22 +122,37 @@ public final class RippleModel implements Comparable<RippleModel> {
         this.received = received;
     }
 
+    /**
+     * Retrieve this model's identifier.
+     */
     public Object getId() {
         return id;
     }
 
+    /**
+     * Retrieve this model's origin.
+     */
     public String getOrigin() {
         return origin;
     }
 
+    /**
+     * Retrieve this model's subject.
+     */
     public String getSubject() {
         return subject;
     }
 
+    /**
+     * Retrieve this model's content.
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * Retrieve when this model's contents were received.
+     */
     public long getReceived() {
         return received;
     }
